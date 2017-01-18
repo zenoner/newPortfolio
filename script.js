1a0454f2c3d33052d7e129d5ba3d8b3d28a6556d
seiji mendoza <srm5149@gmail.com>
2017-01-17 21:23:30 -0800

    updating syle

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("script loaded")
+// $(document).on('turbolinks:load', function(){
+//   $("#sticker").css("background-color", "red");
+// });
+
+$(document).ready(function(){ //>>>jQuery function<<<
+  // $("#sticker").css("background-color", "red");
+
+
+
+})
