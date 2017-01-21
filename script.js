56af6dd6f13b07d69743c86a8f370bc795444a6e
seiji mendoza <srm5149@gmail.com>
2017-01-20 17:21:30 -0800

    completed images

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log("script loaded")
 // });
 
 function myFunction(){
-  console.log("it is working")
+
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -13,7 +13,54 @@ function myFunction(){
   }
 }
 
+$(document).ready(function(){
+   $('.switch').mouseenter(function(){
+      console.log("i got you")
+      $('img.screenshot', this).hide();
+      $('.info', this).show()
+      if ($('.lang', this).is(':hidden')){
+        $('.lang').fadeIn('slow')
+      } else {
+        console.log('dont have')
+      }
+    });
 
-$(document).ready(function(){ //>>>jQuery function<<<
-  // $("#sticker").css("background-color", "red");
-})
+   $('.switch').mouseleave(function(){
+      $('img.screenshot').show();
+      $('.info').hide();
+      $('.lang').hide();
+    });
+
+
+   $('a[href^="#"]').on('click', function(event) {
+    event.preventDefault();
+   $('html, body').animate({
+       scrollTop: $(".portfolio").offset().top
+   }, 1000);
+ });
+
+
+    // $("#about").click(function(){
+    //     console.log("this is working")
+    //     $(this).animate({down: '250px'});
+    // });
+
+    //$("#about").animate({down: '250px'})
+
+
+
+});
+
+
+
+// $(document).ready(function(){ //>>>jQuery function<<<
+//   // $("#sticker").css("background-color", "red");
+//   $(".icon").click(function(){
+//     //  $(".test").slideToggle("slow", function(){
+
+//     // })
+//   });
+// //   $(".icon").click(function(){
+// //     $(".test").slideUp();
+// // })
+// });
